cf59a8a7569714c560e3b1a1b99da6f89cfbd70a
07Nurcahyo <bagusnurcahyo4@gmail.com>
2026-01-28 09:22:08 +0700

    belajar list api menggunakan api pokemon

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,43 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+
+// name:bulbasaur  https://pokeapi.co/api/v2/pokemon/1
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
 
 export default function Index() {
+  // membuat variable 
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    // fetch api
+    fetchPokemons()
+  }, [])
+
+  async function fetchPokemons() {
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?limit=20"
+      );
+      const data = await response.json();
+
+      console.log(data);
+      setPokemons(data.results);
+    } catch(e){
+      console.log(e);
+    }
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView>
+      {pokemons.map((pokemon) => (
+        <View key={pokemon.name}>
+          <Text>{pokemon.name}</Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
